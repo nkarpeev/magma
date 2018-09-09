e4569514a3343ec506b494f31ad700ceca8b0838
maxgromilov <maxgromilov@icloud.com>
2018-09-09 18:23:26 +0300

    layout

--- layout/app/js/common.js
+++ layout/app/js/common.js
@@ -1,5 +1,45 @@
 $(function() {
 
-	// Custom JS
+  $(".catalog__item-button--open, .catalog__item-button--close").on("click", function () {
+    $(this).closest(".catalog__item").toggleClass("open");
+  });
+
+  $('.certificates__item__link').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		mainClass: 'mfp-img-mobile',
+		image: {
+			verticalFit: true
+		}
+		
+  });
+  
+  function svgProcess() {
+    $('.svg').each(function () {
+        var $img = jQuery(this);
+        var imgURL = $img.attr('src');
+        var attributes = $img.prop("attributes");
+        $.get(imgURL, function (data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+            // Remove any invalid XML tags
+            $svg = $svg.removeAttr('xmlns:a');
+            // Loop through IMG attributes and apply on SVG
+            $.each(attributes, function () {
+                $svg.attr(this.name, this.value);
+            });
+            // Replace IMG with SVG
+            $img.replaceWith($svg);
+        }, 'xml').done(function () {
+            if (typeof updateCorpus !== "undefined") {
+                updateCorpus();
+            }
+        });
+    });
+}
+
+svgProcess();
+
+  
 
 });
--- layout/gulpfile.js
+++ layout/gulpfile.js
@@ -39,6 +39,7 @@ gulp.task('js', function() {
 		'app/libs/jquery/dist/jquery.min.js',
     'app/libs/fotorama/fotorama.js',
     'app/libs/bootstrap/js/bootstrap.min.js',
+    'app/libs/popup/jquery.magnific-popup.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
