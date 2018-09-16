86f72cc5aa453fb9404ae590922d6c198ae4ea2d
maxgromilov <maxgromilov@icloud.com>
2018-09-16 19:26:26 +0300

    php

---
+++ layout/app/parts/header.php
@@ -0,0 +1,15 @@
+  <section class="header" id="header" style="background-image: url('img/bg/Bagkround.jpg');">
+    <div class="container">
+      <div class="row header__row">
+        <div class="col">
+          <div class="header__title">
+            ОТ НАБРОСКА<br>
+            К ГОТОВОМУ ИЗДЕЛИЮ
+          </div>
+          <div class="header__btn-wrap">
+            <a href="portfolio.php" class="button">ПОСМОТРЕТЬ ПРОЕКТЫ</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
---
+++ layout/app/parts/product-nav.php
@@ -0,0 +1,61 @@
+  <section class="product-nav" id="product-nav">
+    <div class="container">
+      <div class="row product-nav__row">
+        
+        <a class="col col-lg-2 col-md-4 col-6 product-nav__item active" href="our_product.php">
+          <div class="product-nav__item__icon">
+              <img class="svg" src="img/svg/office.svg" alt="">
+          </div>
+          <div class="product-nav__item__title">
+              Офисная<br>мебель
+          </div>
+        </a>
+        <a class="col col-lg-2 col-md-4 col-6 product-nav__item" href="our_product.php">
+          <div class="product-nav__item__icon">
+            <img class="svg" src="img/svg/sofa.svg" alt="">
+          </div>
+          <div class="product-nav__item__title">
+              Офисные<br>диваны
+          </div>
+        </a>
+
+        <a class="col col-lg-2 col-md-4 col-6 product-nav__item" href="our_product.php">
+            <div class="product-nav__item__icon">
+                <img class="svg" src="img/svg/metalic.svg" alt="">
+            </div>
+            <div class="product-nav__item__title">
+                Металлическая<br>мебель
+            </div>
+        </a>
+
+        <a class="col col-lg-2 col-md-4 col-6 product-nav__item" href="our_product.php">
+          <div class="product-nav__item__icon">
+            <img class="svg" src="img/svg/bath.svg" alt="">
+          </div>
+            <div class="product-nav__item__title">
+                Мебель<br>для ванной
+            </div>
+        </a>
+
+      
+        <a class="col col-lg-2 col-md-4 col-6 product-nav__item" href="our_product.php">
+          <div class="product-nav__item__icon">
+            <img class="svg" src="img/svg/kids.svg" alt="">
+          </div>
+          <div class="product-nav__item__title">
+              Детская<br>мебель
+          </div>
+        </a>
+          
+        <a class="col col-lg-2 col-md-4 col-6 product-nav__item" href="our_product.php">
+          <div class="product-nav__item__icon">
+            <img class="svg" src="img/svg/discount.svg" alt="">
+          </div>
+          <div class="product-nav__item__title">
+              Распродажа<br>мебели
+          </div>
+        </a>
+
+      </div>
+    </div>
+  </section>
